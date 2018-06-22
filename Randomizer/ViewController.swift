cc9a8c2ae2ed0123784e242e67be2a937bd4ae82
EMCEES <office@integral.productions>
2018-06-22 09:06:34 -0600

    Initial Commit

---
+++ Randomizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Randomizer
+//
+//  Created by CELLFiY on 6/22/18.
+//  Copyright © 2018 Matt Schweppe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
